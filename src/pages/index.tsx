b67b51526d4d0630ee6a3e4973ff7289ad03eefe
Luis Moromisato <moromisato.luis@gmail.com>
2021-02-25 07:02:49 -0300

    added new components to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
+import { CompletedChallanges } from "../components/CompletedChallanges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallanges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
